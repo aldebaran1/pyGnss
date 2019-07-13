9f7c5b2b8b325607092afddfd0a0bdc3b250a047
aldebaran1 <sebastijan.mrak@gmail.com>
2019-07-13 17:52:18 -0400

    Download sp3 files

--- utils/batch_tid.sh
+++ utils/batch_tid.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-CONVDIR=/usr3/graduate/smrak/pyTID/pytid/
-
-NAVDIR=/projectnb/semetergrp/tid/nav/
-SBDIR=/projectnb/semetergrp/tid/jplg/
-OBSDIR=/projectnb/semetergrp/tid/obs/
-HDFDIR=/projectnb/semetergrp/tid/hdf/
+NAVDIR=/media/smrak/gnss/nav/
+SBDIR=/media/smrak/gnss/jplg/
+OBSDIR=/media/smrak/gnss/obs/
 
 if [ $1 ]; then
   date=$1
@@ -22,26 +19,27 @@ FN=$OBSDIR$year/$doy/
 FNYAML=$OBSDIR$year/all$doy.yaml
 FNYAMLC=$OBSDIR$year/conus$doy.yaml
 
-#echo Downloading 1/7:
-#python download_rnxi.py $ddt $SBDIR
-#python download_rnxn.py $ddt $NAVDIR
-#python download_rnxo.py $ddt conus $OBSDIR
-#echo Unzipping 2/7:
-#python unzip_rm.py $FN
-#echo ==============================
-#echo listing receiver locations 3/7 from $FN
-#python rxlist.py $FN
-#echo ==============================
-#echo filter receiver locations 4/7
-#echo $FN'rxlist'$doy.$year.h5 $FNYAML
-#python rxfilter.py $FN'rxlist'$doy.$year.h5 $FNYAML
-#python rxfilter.py $FN'rxlist'$doy.$year.h5 $FNYAMLC --lonlim -140 -50 --latlim 0 65
-#echo ==============================
-#echo Converting 5/7
-#python rnx2nc.py $FN 
-#echo Deleting unconverted and rnx/crx
-#rm -rf $FN*.*d
-#rm -rf $FN*.*o
+echo Downloading 1/7:
+python download_rnxi.py $ddt $SBDIR
+python download_rnxn.py $ddt $NAVDIR
+python download_rnxn.py $ddt $NAVDIR --type sp3
+python download_rnxo.py $ddt conus $OBSDIR
+echo Unzipping 2/7:
+python unzip_rm.py $FN
+echo ==============================
+echo listing receiver locations 3/7 from $FN
+python rxlist.py $FN
+echo ==============================
+echo filter receiver locations 4/7
+echo $FN'rxlist'$doy.$year.h5 $FNYAML
+python rxfilter.py $FN'rxlist'$doy.$year.h5 $FNYAML
+python rxfilter.py $FN'rxlist'$doy.$year.h5 $FNYAMLC --lonlim -140 -50 --latlim 0 65
+echo ==============================
+echo Converting 5/7
+python rnx2nc.py $FN
+echo Deleting unconverted and rnx/crx
+rm -rf $FN*.*d
+rm -rf $FN*.*o
 #echo ==============================
 #echo Processing 6/7 to tid
-echo python $CONVDIR'nc2tec_v1.py' $date $FNYAMLC --ts 120 --elmask 20 --log --cfg $CONVDIR'tid_processing.yaml'
+#echo python $CONVDIR'nc2tec_v1.py' $date $FNYAMLC --ts 120 --elmask 20 --log --cfg $CONVDIR'tid_processing.yaml'
--- utils/download_rnxn.py
+++ utils/download_rnxn.py
@@ -83,27 +83,37 @@ def getRinexNav(date:str = None,
     # Constellation navigation file variable
     sct = {'gps' : 'n',
            'glonass' : 'g',
-           'gallileo' : 'e'}
+           'gallileo' : 'e',
+           'sp3': 'sp3'}
     # Open a connection to the FTP address
     with ftplib.FTP(url[1],'anonymous','guest',timeout=15) as F:
         rpath = url[2] + '/' + year + '/' + doy + '/'
         F.cwd(rpath)
-        urlrx = 'brdc' +doy + '0.' + year[-2:]+sct[const]+'.gz'
+        if const == 'sp3':
+            ts = (dt - datetime(1980, 1,6)).total_seconds()
+            gpsweek = int(ts / 60 /60 / 24 / 7)
+            weekday = dt.weekday() + 1
+            wwwwd = str(gpsweek) + str(weekday)
+            urlrx = 'igs{}.sp3'.format(wwwwd)
+        else:
+            urlrx = 'brdc' + doy + '0.' + year[-2:]+sct[const]+'.gz'
         print (urlrx)
         try:
             # urlrx must in in a format "nnnDDD0.YYo.xxx"
             download(F, urlrx, odir+urlrx)
         except Exception as e:
             print (e)
-            
-    unzip(odir+urlrx)
+    if urlrx.endswith('.gz') or urlrx.endswith('.Z') or urlrx.endswith('.zip'):
+        unzip(odir+urlrx)
+        
+    return
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser()
     p.add_argument('date', type=str, help='Date format YYYY-mm-dd')
     p.add_argument('dir', type=str, help='destination directory')
-    p.add_argument('--sct', type=str, help='constellation type',default='gps')
+    p.add_argument('--type', type=str, help='Navtype type: gps, glonass, gallileo, sp3', default='gps')
     
     P = p.parse_args()
     # Get file
-    getRinexNav(date = P.date, odir = P.dir, const = P.sct)
+    getRinexNav(date = P.date, odir = P.dir, const = P.type)
