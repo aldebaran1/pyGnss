3eaa31e5a1ff53f2540a6b9c6820cac8a811e7ee
aldebaran1 <sebastijan.mrak@gmail.com>
2024-08-12 09:59:49 -0400

    plot h5 list

--- utils/rxmap.py
+++ utils/rxmap.py
@@ -25,15 +25,6 @@ def getCoord(fn):
 
 def plotMap(fn,lonlim=None,latlim=None, projection='stereo', save = False):
     if os.path.splitext(fn)[1] in ('.h5', '.hdf5'):
-        root, fname = os.path.split(fn)
-        if latlim is None:
-            latlim = [-89.5,89.5]
-        if lonlim is None:
-            lonlim = [-179.5,180]
-#        else:
-        latlim = array(latlim, dtype=float)
-        lonlim = array(lonlim, dtype=float)
-        gm.plotCartoMap(latlim=latlim,lonlim=lonlim, projection='merc')
         lon, lat = getCoord(fn)
     elif os.path.splitext(fn)[1] in ('.yaml', '.yml'):
         root, fname = os.path.split(fn)
@@ -41,33 +32,33 @@ def plotMap(fn,lonlim=None,latlim=None, projection='stereo', save = False):
         data = array(stream['rx'])
         lon = data[:, 1].astype(float16)
         lat = data[:, 2].astype(float16)
-        # Limits
-        if lonlim is None:
-            
-            X = nanmax(lon)
-            x = nanmin(lon)
-            if X > 150 and x < -150:
-                x0 = X - 5 if X-5 < -180 else -180
-                x1 = x + 5 if x+5 < 180 else 180
-                
-            else:
-                x0 = x - 5 if x-5 < -180 else -180
-                x1 = X + 5 if X+5 < 180 else 180
-            lonlim = [round(x0), round(x1)]
-        if latlim is None:
-            
-            y = nanmin(lat)
-            Y = nanmax(lat)
-            y0 = y-2 if y-2 > -90 else -90
-            y1 = Y+2 if Y+2 < 90 else 90
-            
-            latlim = [round(y0), round(y1)]
-        fig, ax = gm.plotCartoMap(latlim=latlim,lonlim=lonlim, projection=projection,
-                        states=1, background_color='gray')
+        
     else:
         print ('Wrong file format')
         return
-    
+    # Limits
+    if lonlim is None:
+        
+        X = nanmax(lon)
+        x = nanmin(lon)
+        if X > 150 and x < -150:
+            x0 = X - 5 if X-5 < -180 else -180
+            x1 = x + 5 if x+5 < 180 else 180
+            
+        else:
+            x0 = x - 5 if x-5 < -180 else -180
+            x1 = X + 5 if X+5 < 180 else 180
+        lonlim = [round(x0), round(x1)]
+    if latlim is None:
+        
+        y = nanmin(lat)
+        Y = nanmax(lat)
+        y0 = y-2 if y-2 > -90 else -90
+        y1 = Y+2 if Y+2 < 90 else 90
+        
+        latlim = [round(y0), round(y1)]
+    fig, ax = gm.plotCartoMap(latlim=latlim,lonlim=lonlim, projection=projection,
+                    states=1, background_color='gray')
     ax.set_title(fname)
     ax.scatter(lon,lat, marker='.', c='r', s=25, transform=ccrs.PlateCarree())
     print ('Total {} of receivers'.format(lat.size))
